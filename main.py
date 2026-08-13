7f9492fabf51ad6be35f5f6c5b4b70c5fa0010c1
priyaparmar262006-ux <priyaparmar262006@gmail.com>
2026-08-13 08:16:02 +0530

    Add average marks

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from students import students
 
 for student in students:
+    marks = student["marks"]
+    average = sum(marks) / len(marks)
+
     print("Name:", student["name"])
     print("Enrollment:", student["enrollment"])
     print("Marks:", student["marks"])
+    print("Average:", average)
     print()
