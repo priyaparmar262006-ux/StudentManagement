8fcfc7136731fcf6d343067a6710ca601097dad8
priyaparmar262006-ux <priyaparmar262006@gmail.com>
2026-08-13 08:10:42 +0530

    Add student information

---
+++ students.py
@@ -0,0 +1,12 @@
+students = [
+    {
+        "name": "Rahul",
+        "enrollment": "101",
+        "marks": [80, 75, 90]
+    },
+    {
+        "name": "Priya",
+        "enrollment": "102",
+        "marks": [85, 90, 88]
+    }
+]
