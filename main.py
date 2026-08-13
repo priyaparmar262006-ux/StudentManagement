63cf14a27f32a670a02ae3a3b059c9034775cf24
priyaparmar262006-ux <priyaparmar262006@gmail.com>
2026-08-13 08:31:14 +0530

    Add grade calculation

--- main.py
+++ main.py
@@ -21,3 +21,15 @@ else:
 
     if not found:
         print("Student not found")
+
+    def grade(mark):
+        if mark >= 90:
+            return "A"
+        elif mark >= 80:
+            return "B"
+        elif mark >= 70:
+            return "C"
+        elif mark >= 60:
+            return "D"
+        else:
+            return "F"
