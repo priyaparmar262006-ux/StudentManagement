2109c3ac5ca831f5b1073b1828dd60a61c69c5ae
priyaparmar262006-ux <priyaparmar262006@gmail.com>
2026-08-13 08:22:27 +0530

    Improve input validation

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from students import students
 
-for student in students:
-    marks = student["marks"]
-    average = sum(marks) / len(marks)
+name = input("Enter student name: ")
 
-    print("Name:", student["name"])
-    print("Enrollment:", student["enrollment"])
-    print("Marks:", student["marks"])
-    print("Average:", average)
-    print()
+if name.strip() == "":
+    print("Student name cannot be empty")
+else:
+    found = False
+
+    for student in students:
+        if student["name"].lower() == name.lower():
+            marks = student["marks"]
+            average = sum(marks) / len(marks)
+
+            print("Name:", student["name"])
+            print("Enrollment:", student["enrollment"])
+            print("Marks:", student["marks"])
+            print("Average:", average)
+
+            found = True
+
+    if not found:
+        print("Student not found")
