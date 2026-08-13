400474f3a9800fd08a787b90875827048f53c0a2
priyaparmar262006-ux <priyaparmar262006@gmail.com>
2026-08-13 08:13:00 +0530

    Add student display functionality

---
+++ main.py
@@ -0,0 +1,7 @@
+from students import students
+
+for student in students:
+    print("Name:", student["name"])
+    print("Enrollment:", student["enrollment"])
+    print("Marks:", student["marks"])
+    print()
